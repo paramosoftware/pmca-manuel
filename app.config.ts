9d2e43e6ba4dfec1c6c892ec81b1ae5aa977d82b
Matheus <78484194+mthsps@users.noreply.github.com>
2023-04-29 22:30:57 -0300

    Added Nuxt Icon

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+    nuxtIcon: {
+      size: '32px', // default <Icon> size applied
+      class: 'icon' // default <Icon> class applied
+    }
+  })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     modules: [
         "@vuestic/nuxt",
-        '@nuxtjs/tailwindcss',
+        "@nuxtjs/tailwindcss",
+        "nuxt-icon"
     ]
 })
