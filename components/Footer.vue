a6773810b2d39e5cb30f7bdfe45a32fbb73e64d5
Matheus <78484194+mthsps@users.noreply.github.com>
2023-05-01 10:59:30 -0300

    Created the first version of the layout of the footer component

---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="m-5 bg-red-900">
+        <div class="container max-w-screen-xl mx-auto p-5 ">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <div class="flex flex-col">
+                    <p class="text-md sm:text-xl text-white text-start sm:text-center">Thesaurus de conservação-restauração</p>
+                    <Icon name="ci:github" class="text-white p-1" />
+                    <div class="text-start mt-5">
+                        <p class="text-sm  text-white text-start">Projeto Manuel Correia de Andrade (PMCA)</p>
+                        <p class="text-sm  text-white text-start">Desenvolvimento por Páramo </p>
+                    </div>
+                </div>
+                <div class="flex mt-5 space-x-6 sm:justify-center sm:mt-0">
+                        <img width="604" height="96"
+                            src="https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-1024x163.png"
+                            loading="lazy"
+                            srcset="https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-1024x163.png 1024w, 
+                            https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-300x48.png 300w, 
+                            https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-768x122.png 768w, 
+                            https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-1536x245.png 1536w, 
+                            https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-2048x327.png 2048w, 
+                            https://sites.usp.br/pmca/wp-content/uploads/sites/1185/2019/06/Regua-de-Logos-PMCA-V3-1-400x64.png 400w"
+                            sizes="(max-width: 604px) 100vw, 604px">
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
