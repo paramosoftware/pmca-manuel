7555b9c83d20658a69bf0e0d571c4b3158506c2e
Matheus <78484194+mthsps@users.noreply.github.com>
2023-05-03 02:00:43 -0300

    Increased search input width size

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,12 +8,12 @@
       <div class="flex items-center">
         <form id="search">
           <label for="default-search" class="mb-2 text-sm font-medium text-black sr-only">Search</label>
-          <div class="relative">
+          <div class="relative md:w-[500px]">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <Icon name="ph:magnifying-glass" class="text-black w-5 h-5" />
             </div>
             <input type="search" id="default-search"
-              class="block w-full lg:w-auto p-4 pl-10 text-sm text-black border-black bg-transparent focus: focus:outline-none  focus:ring-2 focus:ring-black focus:border-transparent"
+              class="w-full p-4 pl-10 text-sm text-black border-black bg-transparent focus: focus:outline-none  focus:ring-2 focus:ring-black focus:border-transparent"
               required>
             <button type="submit" class="text-black absolute right-2.5 bottom-2.5 text-sm px-4 py-2">Buscar</button>
           </div>
