e24197a4d997751594c19523084e8ba307c095ef
Matheus <dmatheusps@gmail.com>
2023-06-14 03:12:52 -0300

    Added viewing and sorting modes in the list of entries

--- components/external/EntryList.vue
+++ components/external/EntryList.vue
@@ -2,10 +2,10 @@
     <div class="w-full py-4 border-b last:border-b-0 border-red-900">
         <a :href="link" class="w-full h-full flex items-center justify-between">
             <div>
-                <span v-if="entry.category != null">
+                <span v-if="entry.category != null && showCategory">
                     <p class="text-red-900 text-md uppercase">{{ entry.category.name }}</p>
                 </span>
-                <h1 class="text-4xl text-black">{{ entry.name }}</h1>
+                <h1 class="text-3xl text-black">{{ entry.name }}</h1>
                 <p class="text-xl text-black mt-4">
                     <span v-if="entry.translations > 0">
                         <span v-for="translation in entry.translations" :key="translation.id">
@@ -15,7 +15,7 @@
                     </span>
                 </p>
                 <p class="text-md text-gray-500">
-                    {{ entry.definition?.length > 300 ? entry.definition.substring(0, 300) + '...' : entry.definition }}
+                    {{ entry.definition?.length > 130 ? entry.definition.substring(0, 130) + '...' : entry.definition }}
                 </p>
             </div>
             <div class="">
@@ -32,6 +32,10 @@ const props = defineProps({
     entry: {
         type: Object,
         required: true
+    },
+    showCategory: {
+        type: Boolean,
+        default: true
     }
 })
 
--- pages/verbetes/index.vue
+++ pages/verbetes/index.vue
@@ -1,109 +1,192 @@
 <template>
-    <ExternalNavbar />
-    <main class="container max-w-screen-xl mx-auto p-5 bg-white border border-neutral mt-5">
-      <div class="flex items-center justify-between">
-        <div class="flex flex-col">
-          <h1 class="text-5xl text-black">Verbetes</h1>
-          <p class="text-black">{{ filteredEntries.length }} resultados</p>
-        </div>
-  
-        <div class="flex flex-col items-end justify-center">
-          <Icon name="bi:sort-alpha-up" class="text-black" />
-        </div>
+  <ExternalNavbar />
+  <main class="container max-w-screen-xl mx-auto p-5 bg-white border border-neutral mt-5">
+    <div class="flex items-center justify-between">
+      <div class="flex flex-col">
+        <h1 class="text-5xl text-black">Verbetes</h1>
+        <p class="text-black">{{ filteredEntries.length }} resultados</p>
       </div>
-  
-  
-      <div class="mt-4">
+
+      <button class="flex flex-col items-end justify-center" @click="handleSort">
+        <Icon class="text-black w-20" :name="sortOrder === 'asc' ? 'ph:sort-ascending' : 'ph:sort-descending'" />
+      </button>
+    </div>
+
+    <div class="flex flex-col md:flex-row mt-4">
+      <div class="w-full md:w-7/10">
         <label class="text-lg uppercase text-red-900" for="filter">
           Filtrar
         </label>
-        <input v-model="filter" @input=handleInput
-          class="w-full text-sm text-black border-black bg-transparent focus:outline-none focus:ring-red-900 focus:border-transparent"
+        <input v-model="filter" @input=handleFilter
+          class="w-full text-md text-black border-black bg-transparent placeholder:text-gray-500 focus:outline-none focus:ring-red-900 focus:border-transparent"
           id="filter" type="text" placeholder="Filtrar verbetes">
       </div>
-  
-      <Pagination v-if="totalPages > 1" :currentPage="currentPage" :totalPages="totalPages"
-        @update:currentPage="updateCurrentPage" />
-  
-      <span v-if="filteredEntries.length === 0" class="text-black text-xl">
-        Nenhum resultado encontrado
-      </span>
-      <span v-else>
-        <ExternalEntryList v-for="entry in filteredEntries" :key="entry.id" :entry="entry" />
-      </span>
-  
-      <Pagination v-if="totalPages > 1" :currentPage="currentPage" :totalPages="totalPages"
-        @update:currentPage="updateCurrentPage" />
-  
-    </main>
-  
-  
-    <Footer />
-  </template>
+
+      <div class="w-full sm:w-1/10 mt-4 md:mt-0 ml-5">
+        <label class="text-lg uppercase text-red-900" for="list-mode">
+          Modo de exibição
+        </label>
+        <select v-model="listMode" @input=handleMode
+          class="w-full  text-md text-black border-black bg-transparent focus:outline-none focus:ring-red-900 focus:border-transparent"
+          id="list-mode">
+          <option value="category">Por categoria</option>
+          <option value="alphabetical">Alfabética</option>
+          <option value="list">Lista</option>
+        </select>
+      </div>
+    </div>
+
+
+    <span v-if="filteredEntries.length === 0" class="text-black text-xl">
+      Nenhum resultado encontrado
+    </span>
+
+    <div v-if="listMode !== 'list'" id="accordion-flush" data-accordion="collapse" class="mt-5"
+      data-active-classes="text-black" data-inactive-classes="text-black">
+
+      <div v-for="(entries, key, index) in filteredEntriesByMode">
+        <h2 :key="key" :id="'accordion-flush-heading-' + index" class="cursor-pointer mb-5">
+          <button type="button"
+            class="flex items-center justify-between border-b border-b-red-900 w-full p-5 text-4xl text-black text-left bg-white"
+            :data-accordion-target="'#accordion-flush-body-' + index" aria-expanded="false"
+            :aria-controls="'accordion-flush-body-' + index">
+            <span>{{ key }}</span>
+            <Icon data-accordion-icon class="w-20 shrink-0 text-black" name="ph:caret-down" />
+          </button>
+        </h2>
+
+        <div :id="'accordion-flush-body-' + index" class="hidden px-10"
+          :aria-labelledby="'accordion-flush-heading-' + index">
+          <ExternalEntryList v-for="entry in entries" :key="entry.id" :entry="entry"
+            :show-category="listMode !== 'category'" class="pl-4" />
+        </div>
+
+      </div>
+    </div>
+
+    <div v-else class="mt-5">
+      <ExternalEntryList v-for="entry in filteredEntries" :key="entry.id" :entry="entry" />
+    </div>
+
+
+  </main>
+
+
+  <Footer />
+</template>
    
-  <script setup lang="ts">
-  
-  const router = useRouter();
-  
-  const query = computed(() => {
-    return router.currentRoute.value.query;
+<script setup lang="ts">
+
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => {
+  initFlowbite();
+})
+
+onUpdated(() => {
+  initFlowbite();
+})
+
+const router = useRouter();
+
+const query = computed(() => {
+  return router.currentRoute.value.query;
+});
+
+const entries = ref([])
+const filter = ref('');
+const listMode = ref('category'); // category, alphabetical, list
+const sortOrder = ref('asc');
+
+const fetchEntries = async (query) => {
+  const { data } = await useFetchWithBaseUrl('/api/entries/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: query
+    })
   });
-  
-  const entries = ref([])
-  
-  const fetchEntries = async (query) => {
-    const { data } = await useFetchWithBaseUrl('/api/entries/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query: query
-      })
+
+  entries.value = data.value;
+}
+
+fetchEntries(query.value);
+
+// TODO: for some reason, watch is not triggered when the URL is changed directly, 
+// so the fetchEntries need to be called at least once
+watch(() => query.value, async (newQuery) => {
+  await fetchEntries(newQuery)
+});
+
+const filteredEntries = computed(() => {
+  if (!filter.value) {
+    return entries.value;
+  }
+
+  return entries.value?.filter(entry => entry.name.toLowerCase().includes(filter.value.toLowerCase()));
+});
+
+
+const filteredEntriesByMode = computed(() => {
+
+  if (listMode.value === 'alphabetical') {
+    return filteredEntries.value.reduce((acc, entry) => {
+      const letter = entry.name[0].toUpperCase();
+      if (!acc[letter]) {
+        acc[letter] = [];
+      }
+      acc[letter].push(entry);
+      return acc;
+    }, {});
+
+  } else if (listMode.value === 'category') {
+
+    const groupedEntries = filteredEntries.value.reduce((acc, entry) => {
+      const category = entry.category ? entry.category.name : 'Sem categoria';
+      if (!acc[category]) {
+        acc[category] = [];
+      }
+      acc[category].push(entry);
+      return acc;
+    }, {});
+
+
+    const sortedKeys = Object.keys(groupedEntries).sort((a, b) => {
+      if (sortOrder.value === 'asc') {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      } else {
+        return b.toLowerCase().localeCompare(a.toLowerCase());
+      }
     });
-  
-    entries.value = data.value;
+
+    const sortedGroupedEntries = {};
+
+    sortedKeys.forEach(key => {
+      sortedGroupedEntries[key] = groupedEntries[key];
+    });
+
+    return sortedGroupedEntries;
+
   }
+
+});
+
+const handleFilter = (event: Event) => {
+  filter.value = event.target.value;
+};
+
+const handleMode = (event: Event) => {
+
+  listMode.value = event.target.value;
+
+};
+
+const handleSort = () => {
+  sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc';
+  entries.value = entries.value.reverse();
+};
+
+</script>
   
-  fetchEntries(query.value);
-  
-  // TODO: for some reason, watch is not triggered when the URL is changed directly, 
-  // so the fetchEntries need to be called at least once
-  watch(() => query.value, async (newQuery) => {
-    await fetchEntries(newQuery)
-  });
-  
-  const filter = ref('');
-  
-  const handleInput = (event: Event) => {
-    filter.value = event.target.value;
-  };
-  
-  const filteredEntries = computed(() => {
-    if (!filter.value) {
-      return entries.value;
-    }
-  
-    return entries.value?.filter(entry => entry.name.toLowerCase().includes(filter.value.toLowerCase()));
-  });
-  
-  const totalResults = computed(() => filteredEntries.value?.length)
-  
-  const currentPage = ref(1)
-  
-  // TODO: Paginate results and fix pagination
-  
-  const totalPages = computed(() => {
-    if (totalResults.value === 0) {
-      return 1
-    }
-    return Math.ceil(totalResults.value / 10)
-  })
-  
-  const updateCurrentPage = (newPage: number) => {
-    currentPage.value = newPage
-  }
-  
-  </script>
-   
-  <style scoped></style>
