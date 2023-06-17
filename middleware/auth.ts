b93056318c85e8b8f825a0bf88d3e0f8d4ca66c0
Frederico Camargo <fredcamargo@yahoo.com>
2023-06-17 16:14:59 -0300

    PMCA-85 Implementar função de logout

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -12,4 +12,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         return navigateTo('/login');
     }
     
+    if (to.path == '/logout') {
+        return navigateTo('/logout');
+    }
 });
