f1b195e82a0caed8f9c52d67b10a64cef378ab84
Matheus <dmatheusps@gmail.com>
2023-08-08 14:42:56 -0300

    Transformed image space into a carousel on the entry page

--- components/public/EntryMedia.vue
+++ components/public/EntryMedia.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="container">
         <Viewer :images="images" @inited=inited class="viewer">
-            <div class="snap-x w-full overflow-x-auto flex mt-0 sm:mt-0 scrollbar-hide h-60">
-                <img v-for="image in images" :src="image" :key="image" class="snap-start p-2 flex-none object-contain">
-            </div>
+            <Splide :options="options">
+                <SplideSlide v-for="image in images" :key="image">
+                    <img :src="image" class="flex-none object-contain">
+                </SplideSlide>
+            </Splide>
         </Viewer>
     </div>
 </template>
 
-
 <script setup lang="ts">
 import 'viewerjs/dist/viewer.css'
+import '@splidejs/vue-splide/css';
 import { component as Viewer } from "v-viewer"
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
 
 defineProps({
     images: {
@@ -26,4 +29,29 @@ const viewer = ref(null)
 const inited = (viewerInstance) => {
     viewer.value = viewerInstance
 }
+
+const options = {
+    type: 'loop',
+    perPage: 5,
+    perMove: 1,
+    gap: '1rem',
+    pagination: false,
+    breakpoints: {
+        640: {
+            perPage: 1,
+            gap: '1rem',
+            arrows: false,
+            pagination: true
+        },
+        768: {
+            perPage: 2,
+            gap: '1rem',
+        },
+        1024: {
+            perPage: 3,
+            gap: '1rem',
+        },
+    },
+};
+
 </script>
