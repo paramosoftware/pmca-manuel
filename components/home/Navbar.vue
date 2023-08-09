6a65fe347830ab118d5e3b884cf8254e8e7b66f8
Matheus <dmatheusps@gmail.com>
2023-08-09 09:53:02 -0300

    Added responsiveness to the navbar

--- components/home/Navbar.vue
+++ components/home/Navbar.vue
@@ -1,53 +1,62 @@
 <template>
-    <nav id="navbar" class="py-4">
-  
-      <div class="container max-w-screen-xl mx-auto border-b-2 pb-4 border-b-pmca-primary">
-  
-        <div class="grid grid-cols-2 md:grid-cols-8 content-end items-end">
-          <div class="mb-1">
-            <NuxtLink to="/">
-              <img src="/logo-pmca.png" alt="Logo" class="w-14 h-14" />
-            </NuxtLink>
-          </div>
-          <div class="col-span-2 md:col-span-6">
-              <div class="flex justify-evenly">
-                <UILink v-for="link in links" :key="link.name" :href="link.path" class="text-2xl">
-                  {{ link.name }}
-                </UILink>
-              </div>
-          </div>
+  <nav id="navbar" class="py-4">
+    <div class="container  mx-auto border-b-2 pb-4 border-b-pmca-primary h-18">
+      <div class="grid grid-cols-2 md:grid-cols-8 content-end items-end">
+        <div class="mb-1">
+          <NuxtLink to="/">
+            <img src="/logo-pmca.png" alt="Logo" class="w-14 h-14" />
+          </NuxtLink>
+        </div>
+        <button @click="showMenu = !showMenu"
+          class="block md:hidden text-right align-middle mr-3"
+          type="button" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+          <Icon name="ph:list" class="w-9 h-9" />
+        </button>
+        <div class="col-span-2 md:col-span-6">
+          <ul class="justify-evenly md:flex text-base mr-3 origin-top"
+            :class="{ 'block absolute top-18 border-b bg-white w-full p-2': showMenu, 'hidden': !showMenu }">
+            <li v-for="link in links" :key="link.name" :class="showMenu && 'py-1'">
+              <UILink :href="link.path" class="text-2xl">
+                {{ link.name }}
+              </UILink>
+            </li>
+          </ul>
         </div>
       </div>
+    </div>
+  </nav> 
+</template>
   
-    </nav>
-  </template>
-  
-  <script setup lang="ts">
-  
-  const router = useRouter();
-  
-  const search = ref(router.currentRoute.value.query.termo || '');
-  
-  
-  const links = ref([
-    {
-      name: 'Verbetes',
-      path: '/verbetes'
-    },
-    {
-      name: 'Sobre',
-      path: ''
-    },
-    {
-      name: 'Software',
-      path: ''
-    },
-    {
-      name: 'PMCA',
-      path: ''
-    }
-    
-  ]);
-  
+<script setup lang="ts">
+const showMenu = ref(false);
 
-  </script>
+if (process.client) {
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      showMenu.value = false;
+    }
+  });
+}
+
+
+const links = ref([
+  {
+    name: 'Verbetes',
+    path: '/verbetes'
+  },
+  {
+    name: 'Sobre',
+    path: ''
+  },
+  {
+    name: 'Software',
+    path: ''
+  },
+  {
+    name: 'PMCA',
+    path: ''
+  }
+]);
+
+
+</script>
--- components/public/Navbar.vue
+++ components/public/Navbar.vue
@@ -9,30 +9,47 @@
         </div>
         <div class="col-span-3 md:col-span-4">
           <div class="flex flex-col justify-between">
-            <div>
-              <h1 class="text-2xl font-semibold flex-row mb-2">
-                Glossário de conservação-restauro de livros e documentos
-              </h1>
-            </div>
-            <div>
-              <UILink v-for="link in links" :key="link.name" :href="link.path" class="text-2xl mr-5">
-                {{ link.name }}
-              </UILink>
-            </div>
+            <h1 class="text-2xl font-semibold flex-row mb-2">
+              Glossário de conservação-restauro de livros e documentos
+            </h1>
           </div>
         </div>
+        <button @click="showMenu = !showMenu" type="button" class="block md:hidden text-right align-middle col-span-4 md:col-span-0 mr-3"
+            aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
+            <Icon name="ph:list" class="w-9 h-9" />
+        </button>
         <div class="col-span-1 md:col-span-1">
         </div>
-        <div class="col-span-4 md:col-span-6 relative">
+        <div class="col-span-4 md:col-span-6 relative mr-3 lg:mr-0">
           <PublicSearchBar />
         </div>
       </div>
+      <ul class="justify-evenly md:flex text-base mr-3 origin-top"
+        :class="{ 'block absolute border-b shadow-sm bg-white w-full p-2 top-52 ': showMenu, 'hidden': !showMenu }">
+        <li v-for="link in links" :key="link.name" :class="showMenu && 'py-1'">
+          <UILink :href="link.path" class="text-2xl">
+            {{ link.name }}
+          </UILink>
+        </li>
+      </ul>
     </div>
-
   </nav>
 </template>
 
 <script setup lang="ts">
+
+const showMenu = ref(false);
+
+
+ if (process.client) {
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      showMenu.value = false;
+    }
+  });
+}
+
+
 
 const links = ref([
   {
