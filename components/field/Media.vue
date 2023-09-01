d9667232731b5b5382716a4f658d412f6c9fad07
Matheus <dmatheusps@gmail.com>
2023-09-01 15:38:12 -0300

    Fixed overlapping images in the media component

--- components/field/Media.vue
+++ components/field/Media.vue
@@ -1,19 +1,21 @@
 <template>
-    <div id="accordion-collapse" class="mt-8" data-accordion="collapse" data-active-classes="text-black"
-        data-inactive-classes="text-black">
-        <h2 :id="'accordion-collapse-heading-' + id" class="cursor-pointer">
+    <div class="mt-8">
+        <h2 class="cursor-pointer"  @click="isOpenAccordion = !isOpenAccordion">
             <button type="button"
-                class="flex items-center justify-between w-full mt-4 text-lg text-pmca-secondary first-letter:uppercase text-left bg-white border border-gray-300 p-2"
-                :data-accordion-target="'#accordion-collapse-body-' + id" aria-expanded="false"
-                :aria-controls="'accordion-collapse-body-' + id">
-                <span>{{ label }}</span>
-                <Icon data-accordion-icon class="w-10 rotate-180 text-gray-300" name="ph:caret-down" />
+                class="flex items-center justify-between w-full mt-4 text-lg text-pmca-secondary first-letter:uppercase text-left bg-white border border-gray-300 p-2">
+                <span>
+                    {{ label }}
+                    <span class="text-sm text-gray-400" v-if="media.length">
+                        ({{ media.length }} 
+                        arquivo{{ media.length > 1 ? 's' : '' }})
+                    </span>
+                </span>
+
+                <Icon class="w-10 text-gray-300" :name="isOpenAccordion ? 'ph:caret-up' : 'ph:caret-down'" />
             </button>
         </h2>
 
-        <div :id="'accordion-collapse-body-' + id" class="hidden p-2 border border-gray-300"
-            :aria-labelledby="'accordion-collapse-heading-' + id">
-
+        <div class="p-2 border border-gray-300" v-show="isOpenAccordion">
             <div class="mb-4 text-right">
                 <UIButton label="ADICIONAR ARQUIVOS" @click="isOpen = true" />
                 <UModal v-model="isOpen" :ui="{ width: 'max-w-5xl', rounded: '' }">
@@ -31,8 +33,8 @@
             </div>
 
             <div class="grid grid-cols-6 gap-4">
-                <div v-for="image in media" :key="image" class="relative">
-                    <UIImg class="h-40 max-w-sm" :src="image.name" />
+                <div v-for="image in media" :key="image.id" class="relative">
+                    <UIImg class="w-full h-32 object-cover rounded" :src="image.name" />
                     <button type="button" class="text-black" @click="deleteMedia(image.id)">
                         <Icon class="absolute top-0 right-0 w-6 h-6 bg-white rounded-full" name="ph:trash-simple" />
                     </button>
@@ -45,16 +47,7 @@
 <script setup lang="ts">
 
 const isOpen = ref(false)
-
-import { initFlowbite } from 'flowbite'
-
-onMounted(() => {
-    initFlowbite();
-})
-
-onUpdated(() => {
-    initFlowbite();
-})
+const isOpenAccordion = ref(false)
 
 const props = defineProps({
     id: {
