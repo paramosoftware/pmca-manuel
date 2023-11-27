8510bc20465b09bfc32ab159d7b9d69c8bfa9efd
Matheus <dmatheusps@gmail.com>
2023-11-27 13:38:29 -0300

    refactor(prisma): create app models and reset migration history with new schema

--- server/prisma/migrations/20230620132658_variations/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "variations" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "entry_id" INTEGER NOT NULL,
-    CONSTRAINT "variations_entry_id_fkey" FOREIGN KEY ("entry_id") REFERENCES "entries" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- server/prisma/migrations/20230810011804_entries_normalized/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "entries" ADD COLUMN "definition_normalized" TEXT;
-ALTER TABLE "entries" ADD COLUMN "name_normalized" TEXT;
-ALTER TABLE "entries" ADD COLUMN "notes_normalized" TEXT;
