6640fe4e5d7317b844228280bc03f382dac65958
Matheus <dmatheusps@gmail.com>
2023-12-31 19:18:43 -0300

    feat: add export and import functions of all data as json

--- server/express/prisma/export.ts
+++ server/express/prisma/export.ts
@@ -9,112 +9,95 @@ import { prisma } from '~/server/prisma/prisma';
 import { createOneOrMany } from './create';
 import { saveMedia } from './media';
 import { v4 as uuidv4 } from 'uuid';
+import  deleteFolder from '~/utils/deleteFolder';
 
 
 export async function exportAll() {
 
-    const exportModels = ['category', 'entry'];
+    const model = 'entry';
     const fieldsMap = new Map<string, Prisma.DMMF.Field[]>();
     let mediaFiles = new Map<string, string>();
 
     for (const prismaModel of Prisma.dmmf.datamodel.models) {
         const modelName = prismaModel.name.toLocaleLowerCase();
-        if (exportModels.includes(modelName)) {
+        if (model === modelName) {
             fieldsMap.set(modelName, prismaModel.fields);
         }
     }
-  
-    for (const model of exportModels) {
-        const pageSize = 200;
-        let page = 1;
-        const include = OBJECTS[model].includeRelations;
 
-        const data = await readOneOrManyWithQuery(model, { pageSize, page, include });
+    const pageSize = 200;
+    let page = 1;
+    const include = OBJECTS['verbete'].includeRelations;
 
-        if (!data || data.totalCount === 0) {
+    const data = await readOneOrManyWithQuery(model, { pageSize, page, include });
+
+    if (!data || data.totalCount === 0) {
+        return;
+    }
+
+    const filePath = path.join(process.cwd(), 'server', 'temp', `${model.toLowerCase()}.json`);
+    fs.writeFileSync(filePath, '[');
+
+    for (let i = 0; i < data.totalPages; i++) {
+        const data = await readOneOrManyWithQuery(model, { pageSize, page: i + 1, include });
+
+        if (!data) {
             continue;
         }
 
-        const filePath = path.join(process.cwd(), 'server', 'temp', `${model.toLowerCase()}.json`);
-        fs.writeFileSync(filePath, '[');
+        for (const item of data.data) {
+            const newItem = {} as any;
 
-        for (let i = 0; i < data.totalPages; i++) {
-            const data = await readOneOrManyWithQuery(model, { pageSize, page: i + 1, include });
+            newItem.name = item.name;
+            newItem.nameSlug = item.nameSlug;
+            newItem.definition = item.definition;
+            newItem.parent = item.parent?.nameSlug;
+            newItem.isCategory = item.isCategory;
+            newItem.relatedEntries = item.relatedEntries?.map((entry: Entry) => entry.nameSlug);
+            newItem.references = item.references?.map((reference: Reference) => reference.name);
+            newItem.variations = item.variations?.map((variation : Variation) => variation.name);
+            newItem.translations = item.translations?.map((translation: Translation) => {
+                return {
+                    language: translation.language?.name,
+                    name: translation.name
+                }
+            });
 
-            if (!data) {
-                continue;
+            if (item.media) {
+                for (const media of item.media) {
+                    const fileName = media.media?.name;
+                    const extension = fileName.split('.').pop();
+                    const position = media.position ? media.position : 1;
+                    const newFileName = `${item.nameSlug}_${position}.${extension}`;
+                    mediaFiles.set(fileName, newFileName);
+                }
             }
 
-            for (const item of data.data) {
-                const newItem = {} as any;
-
-                if (model === 'entry') {
-                    newItem.name = item.name;
-                    newItem.slug = item.nameSlug;
-                    newItem.description = item.description;
-                    newItem.category = item.category?.name;
-                    newItem.relatedEntries = item.relatedEntries?.map((entry) => entry.name);
-                    newItem.references = item.references?.map((reference) => reference.name);
-                    newItem.variations = item.variations?.map((variation) => variation.name);
-                    newItem.translations = item.translations?.map((translation) => {
-                        return {
-                            language: translation.language?.name,
-                            name: translation.name
-                        }
-                    });
-
-
-                    if (item.media) {
-                        for (const media of item.media) {
-                            const fileName = media.media?.name;
-                            const extension = fileName.split('.').pop();
-                            const position = media.position ? media.position : 1;
-                            const newFileName = `${item.nameSlug}_${position}.${extension}`;
-                            mediaFiles.set(fileName, newFileName);
-                        }
-                    }
-                }
-
-                if (model === 'category') {
-                    newItem.name = item.name;
-                    newItem.slug = item.nameSlug;
-                    newItem.definition = item.definition;
-                    newItem.parent = item.parent?.name;
-                }
-                
-
-                const json = JSON.stringify(newItem, null, 2);
-                fs.appendFileSync(filePath, json);
-                fs.appendFileSync(filePath, ',');
-                fs.appendFileSync(filePath, '\n');
-
-            }
-
+            const json = JSON.stringify(newItem, null, 2);
+            fs.appendFileSync(filePath, json);
+            fs.appendFileSync(filePath, ',');
+            fs.appendFileSync(filePath, '\n');
         }
-        
-        const fd = fs.openSync(filePath, 'r+');
-        fs.ftruncateSync(fd, fs.statSync(filePath).size - 2);
-        fs.closeSync(fd);
-        fs.appendFileSync(filePath, ']');
-
     }
 
+    const fd = fs.openSync(filePath, 'r+');
+    fs.ftruncateSync(fd, fs.statSync(filePath).size - 2);
+    fs.closeSync(fd);
+    fs.appendFileSync(filePath, ']');
 
-    const zipPath = path.join(process.cwd(), 'server', 'temp', 'export.zip');
+    const zipPath = path.join(process.cwd(), 'server', 'temp', `export.zip`);
     const zip = new Zip();
-
 
     for (const [fileName, newFileName] of mediaFiles) {
         const absoluteFilePath = path.join(process.cwd(), 'public', 'media', fileName);
-        zip.addLocalFile(absoluteFilePath, '', newFileName);        
+        if (fs.existsSync(absoluteFilePath)) {
+            zip.addLocalFile(absoluteFilePath, 'media', newFileName);
+        }
     }
 
-    for (const model of exportModels) {
-        const filePath = path.join(process.cwd(), 'server', 'temp', `${model.toLowerCase()}.json`);
-        zip.addLocalFile(filePath, 'media');
-    }
-
+    zip.addLocalFile(filePath);
     zip.writeZip(zipPath);
+    fs.unlinkSync(filePath);
 
     return zipPath;
 }
@@ -122,54 +105,17 @@ export async function exportAll() {
 
 export async function importAll() {
 
+    const exportModel = 'entry';
     const filePath = path.join(process.cwd(), 'server', 'temp', 'export.zip');
     
     const zip = new Zip(filePath);
     zip.extractAllTo(path.join(process.cwd(), 'server', 'temp', 'import'), true);
-    
 
-    const categoriesFilePath = path.join(process.cwd(), 'server', 'temp', 'import', 'category.json');
-    const categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
-    const categoriesMap = new Map<string, number>();
-    const categoriesParentMap = new Map<string, string>();
-
-
-    await prisma.category.deleteMany({});
-
-    for (const category of categories) {
-        const newCategory = await createOneOrMany('category', {
-            name: category.name,
-            definition: category.definition
-        });
-        
-        categoriesMap.set(category.name, newCategory.id);
-
-        if (category.parent) {
-            categoriesParentMap.set(category.name, category.parent);
-        }
-    }
-
-    for (const [categoryName, parentName] of categoriesParentMap) {
-        await prisma.category.update({
-            where: {
-                name: categoryName
-            },
-            data: {
-                parent: {
-                    connect: {
-                        name: parentName
-                    }
-                }
-            }
-        });
-    }
-
-
-    const entriesFilePath = path.join(process.cwd(), 'server', 'temp', 'import', 'entry.json');
-    const entries = JSON.parse(fs.readFileSync(entriesFilePath, 'utf-8'));
-    const entriesMap = new Map<string, number>();
-    const entriesRelatedEntriesMap = new Map<string, string[]>();
+    const exportFilePath = path.join(process.cwd(), 'server', 'temp', 'import', exportModel + '.json');
+    const entries = JSON.parse(fs.readFileSync(exportFilePath, 'utf-8'));
     const languagesMap = new Map<string, number>();
+    const parentEntriesMap = new Map<string, string>();
+    const entriesRelatedEntriesMap = new Map<string, string[]>();
 
     await prisma.entry.deleteMany({});
     await prisma.language.deleteMany({});
@@ -192,20 +138,20 @@ export async function importAll() {
 
         const newEntry = await createOneOrMany('entry', {
             name: entry.name,
-            nameSlug: entry.slug,
-            definition: entry.description,
-            categoryId: categoriesMap.get(entry.category),
-            references: entry.references.map((reference) => {
+            nameSlug: entry.nameSlug,
+            definition: entry.definition,
+            isCategory: entry.isCategory,
+            references: entry.references.map((reference: Reference) => {
                 return {
                     name: reference
                 }
             }),
-            variations: entry.variations.map((variation) => {
+            variations: entry.variations.map((variation: Variation) => {
                 return {
                     name: variation
                 }
             }),
-            translations: entry.translations?.map((translation) => {
+            translations: entry.translations?.map((translation: { name: string; language: string; }) => {
                 return {
                     name: translation.name,
                     languageId: languagesMap.get(translation.language)
@@ -213,25 +159,26 @@ export async function importAll() {
             })
         });
 
-        entriesMap.set(entry.nameSlug, newEntry.id);
-
         if (entry.relatedEntries && entry.relatedEntries.length > 0) {
-           entriesRelatedEntriesMap.set(entry.name, entry.relatedEntries);
+           entriesRelatedEntriesMap.set(newEntry.nameSlug, entry.relatedEntries);
         }
 
+        if (entry.parent) {
+            parentEntriesMap.set(newEntry.nameSlug, entry.parent);
+        }
     }
 
 
     for (const [entryName, relatedEntries] of entriesRelatedEntriesMap) {
         await prisma.entry.update({
             where: {
-                name: entryName
+                nameSlug: entryName
             },
             data: {
                 relatedEntries: {
                     connect: relatedEntries.map((relatedEntryName) => {
                         return {
-                            name: relatedEntryName
+                            nameSlug: relatedEntryName
                         }
                     })
                 }
@@ -239,16 +186,25 @@ export async function importAll() {
         });
     }
 
+    for (const [entryName, parentName] of parentEntriesMap) {
+        await prisma.entry.update({
+            where: {
+                nameSlug: entryName
+            },
+            data: {
+                parent: {
+                    connect: {
+                        nameSlug: parentName
+                    }
+                }
+            }
+        });
+    }
 
-    const mediaFilePath = path.join(process.cwd(), 'server', 'temp', 'import');
+    const mediaFilePath = path.join(process.cwd(), 'server', 'temp', 'import', 'media');
     const mediaFiles = fs.readdirSync(mediaFilePath);
 
     for (const mediaFile of mediaFiles) {
-
-        if (mediaFile.endsWith('.json')) {
-            continue;
-        }
-
         const parts = mediaFile.split('_');
 
         const extension = parts[1].split('.')[1];
@@ -256,24 +212,26 @@ export async function importAll() {
         const position = parts[1].split('.')[0];
         const newFileName = `${uuidv4()}.${extension}`;
 
-        const entryId = entriesMap.get(fileName)!.toString();
+        const entry = await prisma.entry.findFirst({
+            where: {
+                nameSlug: fileName
+            }
+        });
 
-        await saveMedia(entryId, newFileName, mediaFile, parseInt(position));
+        if (!entry) {
+            continue;
+        }
 
-        const oldPath = path.join(process.cwd(), 'server', 'temp', 'import', mediaFile);
+        await saveMedia(entry.id, newFileName, mediaFile, parseInt(position));
+
+        const oldPath = path.join(process.cwd(), 'server', 'temp', 'import', 'media', mediaFile);
         const newPath = path.join(process.cwd(), 'public', 'media', newFileName);
         fs.renameSync(oldPath, newPath);
     }
 
-    
-
-
-
-    
-    
-
-
-
+    const tempPath = path.join(process.cwd(), 'server', 'temp', 'import');
+    deleteFolder(tempPath);
+    fs.unlinkSync(filePath);
 }
 
 
