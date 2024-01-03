0835939d445c58017ace59481a816ef2aaab67fa
Matheus <dmatheusps@gmail.com>
2024-01-03 16:01:26 -0300

    refactor(upload): create new function for handling upload files with multer

--- server/express/prisma/media.ts
+++ server/express/prisma/media.ts
@@ -3,59 +3,24 @@ import fs from 'fs';
 import multer from 'multer';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
-import getMediaPath from '../../../utils/getMediaPath';
+import getMediaPath from '~/utils/getMediaPath';
+import getTempPath from '~/utils/getTempPath';
 import { prisma } from '../../prisma/prisma';
 import { UploadError } from '../error';
 
 
 export async function uploadMedia(model: string, id: string, body: any, req: express.Request, res: express.Response, next: express.NextFunction) {
 
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, getMediaPath() + '/')
-        },
-        filename: function (req, file, cb) {
-            cb(null, uuidv4() + path.extname(file.originalname))
-        }
-    })
+    const mediaPath = await importUploadFile(req, res, next, getMediaPath(true)) as string;
 
-    const upload = multer({
-        storage: storage,
-        limits: { fileSize: 1024 * 1024 * 5 },
-    }).single('file')
+    if (!mediaPath || !fs.existsSync(mediaPath)) {
+        return next(new UploadError('Error uploading file'));
+    }
 
-    return new Promise((resolve, reject) => {
-        upload(req, res, async (err: any) => {
-            try {
-                if (err) {
-                    throw new UploadError('Error uploading file');
-                }
+    const fileName = path.basename(mediaPath);
+    const mediaData = await saveMedia(id, fileName, req.file?.originalname ?? '');
 
-                if (!req.file) {
-                    throw new UploadError('No file was sent');
-                }
-
-                const data = await saveMedia(id, req.file?.filename, req.file?.originalname);
-
-                resolve(data);
-
-            } catch (error) {
-
-                const filePath = path.join('public', req.file?.filename);
-
-                if (fs.existsSync(filePath)) {
-                    fs.unlink(filePath, (err) => {
-                        if (err) {
-                            throw new UploadError('Error deleting file');
-                        }
-                    });
-                }
-
-                next(reject(error));
-
-            }
-        });
-    });
+    res.json(mediaData);
 }
 
 export async function deleteMedia(entryMedia: Array<EntryMedia>) {
@@ -150,4 +115,63 @@ export async function saveMedia(entryId: string | Number, fileName: string, orig
         throw new UploadError('Error saving media to database');
     }
 
+}
+
+
+export async function importUploadFile(req: express.Request, res: express.Response, next: express.NextFunction, destinationFolder: string = '') {
+
+    if (!destinationFolder) {
+        destinationFolder = getTempPath(true);
+    }
+
+    if (!fs.existsSync(destinationFolder)) {
+        next(new UploadError('Destination folder does not exist'));
+    }
+
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, destinationFolder)
+        },
+        filename: function (req, file, cb) {
+            cb(null, uuidv4() + path.extname(file.originalname))
+        }
+    })
+
+    const upload = multer({
+        storage: storage,
+        limits: { fileSize: 1024 * 1024 * 20 }
+    }).single('file')
+
+
+
+    return new Promise((resolve, reject) => {
+        upload(req, res, async (err: any) => {
+            try {
+                if (err) {
+                    throw new UploadError('Error uploading file');
+                }
+
+                if (!req.file) {
+                    throw new UploadError('No file was sent');
+                }
+
+                resolve(path.join(destinationFolder, req.file?.filename));
+
+            } catch (error) {
+
+                const filePath = path.join(destinationFolder, req.file?.filename);
+
+                if (fs.existsSync(filePath)) {
+                    fs.unlink(filePath, (err) => {
+                        if (err) {
+                            throw new UploadError('Error deleting file');
+                        }
+                    });
+                }
+
+                next(reject(error));
+
+            }
+        });
+    });
 }
