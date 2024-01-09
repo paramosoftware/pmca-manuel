d918baa73b67737aed78df16bdff284cbb44f414
Matheus <dmatheusps@gmail.com>
2024-01-08 22:28:21 -0300

    refactor(forms): create new checkbox field

--- components/field/Checkbox.vue
+++ components/field/Checkbox.vue
@@ -1,36 +1,63 @@
 <template>
-
-    <div class="flex flex-row items-center">
-        <input
-            type="checkbox"
-            class="form-checkbox h-5 w-5 text-pmca-accent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pmca-accent"
-            :id="id"
-            :checked="modelValue"
-            @input="handleInput"
-        />
-        <label :for="id" class="ml-2 block text-pmca-primary">
-            {{ label }}
-        </label>
+    <div class="flex flex-row items-center my-3" v-show="!hidden">
+        <UCheckbox 
+            :id="id" 
+            :model-value="modelValue" 
+            :disabled="disabled"
+            @change="onInput"
+        >
+            <template #label>
+                {{ label  }}
+            </template>
+        </UCheckbox>
     </div>
-    
-
 </template>
 
 <script setup lang="ts">
 
 const props = defineProps({
-    id: String,
-    label: String,
-    modelValue: {
-        type: Boolean,
+    id: {
+        type: String,
         required: true
+    },
+    modelValue: {
+        type: [String, Number, Boolean],
+        default: ''
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    hidden: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    formStore: {
+        type: Object as PropType<FormStore>,
     }
 });
 
+const defaultValue = getFormFieldConfig('defaultValue', '', props);
+const modelValue = getFormFieldConfig('modelValue', defaultValue?.value, props);
+const disabled = getFormFieldConfig('disabled', false, props);
+const hidden = getFormFieldConfig('hidden', false, props);
+const label = getFormFieldConfig('label', '', props);
+
 const emit = defineEmits(['update:modelValue']);
 
-const handleInput = (event: Event) => {
-    const target = event.target as HTMLInputElement;
-    emit('update:modelValue', target.checked);
+const onInput = (event: Event) => {
+
+    const value = (event.target as HTMLInputElement).checked;
+
+    if (props.formStore) {
+        props.formStore.setFieldData(props.id, value);
+    }
+
+    emit('update:modelValue', value);
 }
+
 </script>
