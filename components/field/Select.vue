48c693b810f8e785f91526e5067fc00d8fb9d0cd
Matheus <dmatheusps@gmail.com>
2024-01-09 00:20:43 -0300

    refactor(forms): create new select field

--- components/field/Select.vue
+++ components/field/Select.vue
@@ -1,69 +1,161 @@
 <template>
-    <div class="mt-4">
-        <label class="text-lg text-pmca-secondary first-letter:uppercase" :for="id">
+    <div class="mt-4" :class="{ 'hidden': hidden }">
+        <UILabel :for="id">
             {{ label }}
-        </label>
+        </UILabel>
 
-        <select
-            class="w-full bg-gray-50 border border-gray-200 p-2 focus:outline-none focus:border-pmca-accent rounded-sm"
+        <USelect
             :id="id"
-            @input="handleInput"
+            :model-value="modelValue"
+            :disabled="disabled"
+            :hidden="hidden"
+            :placeholder="placeholder"
+            :required="required"
+            :loading="loading"
+            :options="list"
+            option-attribute="name"
+            @input="onInput"
+            size="md"
+            class="w-full"
+            variant="outline"
+            color="gray"
         >
-
-            <option v-if=!required :value="placeholder.id" disabled selected>
-                {{ placeholder.name }}
-            </option>
-
-            <option v-for="option in options" :key="option.id"
-                class="bg-white hover:bg-gray-100"
-                :value="option.id"
-                :selected="option.id === modelValue.id">
-                {{ option.name }}
-            </option>
-        </select>
+        </USelect>
     </div>
 </template>
 
 <script setup lang="ts">
 
 const props = defineProps({
-    id: String,
-    label: String,
-    modelValue: {
-        type: Object as PropType<{ id: number | string, name: string }>,
+    id: {
+        type: String,
         required: true
     },
-    options: {
-        type: Array as PropType<{ id: number | string, name: string }[]>,
-        required: true,
-        default: () => [{ id: 0, name: '' }]
+    modelValue: {
+        type: [String, Number, Object],
+        default: ''
     },
     required: {
         type: Boolean,
         default: false
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    placeholder: {
+        type: String,
+        default: ''
+    },
+    hidden: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    relatedResource: {
+        type: String,
+        default: ''
+    },
+    options: {
+        type: Array as PropType<{ name: string, value: string | number }[]>,
+        default: []
+    },
+    formStore: {
+        type: Object as PropType<FormStore>,
     }
 });
 
-const emit = defineEmits(['update:modelValue']);
+console.log(props.options);
 
-const handleInput = (event: Event) => {
-    const target = event.target as HTMLInputElement;
+const list = ref<{id?: ID, name: string, value: string | number }[]>([]);
 
-    const id = parseNumber(target.value);
-    const option = props.options.find(option => option.id === id) ?? { id: '', name: '' };
+const defaultValue = getFormFieldConfig('defaultValue', '', props);
+const modelValue = getFormFieldConfig('modelValue', defaultValue?.value, props);
+const disabled = getFormFieldConfig('disabled', false, props);
+const hidden = getFormFieldConfig('hidden', false, props);
+const label = getFormFieldConfig('label', '', props);
+const required = getFormFieldConfig('required', false, props);
+const placeholder = getFormFieldConfig('placeholder', '', props);
+const relatedResource = getFormFieldConfig('relatedResource', '', props);
+const defaultOptions = getFormFieldConfig('defaultOptions', '', props);
+const options = getFormFieldConfig('options', [], props);
 
-    if (props.required && (id === '' || id == '0')) { 
-        const option = props.options[0];
-        emit('update:modelValue', { id: option.id, name: option.name });
-        return;
+const loading = ref(false);
+
+if (relatedResource.value) {
+
+    loading.value = true;
+
+    const { data, pending, error } = await useFetchWithBaseUrl('/api/' + relatedResource.value.name, {
+        method: 'GET',
+        query: { pageSize: 100 },
+        transform: (data: PaginatedResponse) => {
+            if (!data) {
+                return [];
+            } else {
+                return data.items.map((item) => ({
+                    id: item.id,
+                    name: item.label ?? item.name,
+                    value: item.id
+                }))
+            }
+        }
+    });
+
+    loading.value = pending.value;
+
+    if (data.value) {
+        list.value = data.value;
     }
 
-    emit('update:modelValue', { id, name: option.name });
+    if (error.value) {
+        console.error(error.value);
+    }
+
+} else if (defaultOptions.value) {
+
+    const values = defaultOptions.value.split(',');
+
+    for (const option of values) {
+        list.value.push({
+            name: option.trim(),
+            value: option.trim()
+        });
+    }
+
+} else if (options.value) {
+    list.value = options.value;
 }
 
-handleInput({ target: { value: props.modelValue.id } } as unknown as Event);
+if (defaultValue.value && props.formStore) {
+    list.value.map((item) => {
+        if (item.name === defaultValue.value || item?.id === defaultValue.value) {
+            props.formStore?.setFieldData(props.id, item);
+        }
+    });
+}
 
-const placeholder = ref({ id: '', name: '' });
+
+if (!required.value) {
+    list.value.unshift({
+        name: '',
+        value: ''
+    });
+}
+
+const emit = defineEmits(['update:modelValue']);
+
+const onInput = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+
+    if (props.formStore) {
+        props.formStore.setFieldData(props.id, target.value);
+    }
+
+    emit('update:modelValue', target.value);
+}
 
 </script>
-
