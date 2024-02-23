501a27a2cc8888e01f2d0292365a33a920ad581b
Matheus <dmatheusps@gmail.com>
2024-02-23 10:24:45 -0300

    fix(api): update csrfHandler with new decodedJwt method

--- server/express/auth/csrfHandler.ts
+++ server/express/auth/csrfHandler.ts
@@ -1,39 +1,43 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import getCookiePrefix from '~/utils/getCookiePrefix';
-import { UnauthorizedError } from '../error';
+import express from "express";
+import jwt from "jsonwebtoken";
+import getCookiePrefix from "~/utils/getCookiePrefix";
+import decodeJwt from "~/utils/decodeJwt";
+import { UnauthorizedError } from "../error";
 
-const csrfHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+const csrfHandler = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const exceptions = [
+    "/api/auth/login",
+    "/api/auth/refresh",
+    "/api/auth/logout",
+  ];
 
-    // TODO: Review exceptions
-    const exceptions = ['/api/auth/login', '/api/auth/refresh', '/api/auth/logout'];
+  if (req.method !== "GET" && !exceptions.includes(req.path)) {
+    const csrf = req.headers["x-csrf-token"];
 
-    if (req.method !== 'GET' && !exceptions.includes(req.path)) {
-        const csrf = req.headers['x-csrf-token'];
+    const cookieName = getCookiePrefix() + "jwt";
 
-        const cookieName = getCookiePrefix() + 'jwt';
+    const token = req.cookies[cookieName];
 
-        const token = req.cookies[cookieName];
-
-        if (!token || !csrf) {
-            throw new UnauthorizedError('Invalid CSRF token');
-        }
-
-        try {
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, { ignoreExpiration: true, maxAge: '3h' });
-        } catch (error) {
-            //throw new InvalidCredentialError('Invalid CSRF token');
-        }
-
-        const csrfToken = jwt.decode(token) as { csrf: string };
-
-        if (csrfToken.csrf !== csrf) {
-            throw new UnauthorizedError('Invalid CSRF token');
-        }
+    if (!token || !csrf) {
+      throw new UnauthorizedError("Invalid CSRF token");
     }
 
-    next();
-}
+    if (!decodeJwt(token, process.env.ACCESS_TOKEN_SECRET!, true, "5h")) {
+      throw new UnauthorizedError("Invalid CSRF token");
+    }
 
+    const csrfToken = jwt.decode(token) as { csrf: string };
+
+    if (csrfToken.csrf !== csrf) {
+      throw new UnauthorizedError("Invalid CSRF token");
+    }
+  }
+
+  next();
+};
 
 export default csrfHandler;
