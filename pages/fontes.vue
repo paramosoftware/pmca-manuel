99046ed6ae91d0ebd7a4af6ea8dd1a474862e778
Matheus <dmatheusps@gmail.com>
2024-06-04 22:31:20 -0300

    feat(pages): create provisional page to show references

---
+++ pages/fontes.vue
@@ -0,0 +1,36 @@
+<template>
+    <PublicPage title="Fontes">
+        <div class="flex flex-col justify-left">
+            <div class="mt-2">
+                <div
+                    v-for="reference in references"
+                    :key="reference.id"
+                    class="flex flex-row mb-5"
+                    v-html="reference.nameRich"
+                >
+                </div>
+            </div>
+        </div>
+    </PublicPage>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'public'
+});
+
+const { data, pending, error } = (await useFetchWithBaseUrl(
+    `/api/public/reference`,
+    {
+        params: {
+            pageSize: -1
+        }
+    }
+)) as {
+    data: Ref<PaginatedResponse>;
+    pending: Ref<boolean>;
+    error: Ref<Error | undefined>;
+};
+
+const references = ref(data.value.items) as Ref<Reference[]>;
+</script>
