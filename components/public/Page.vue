4ac5e91f129d129573d8efe91cb6a094d796fb28
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-10 17:24:04 -0300

    fix: added default margin on y axis for Page.vue

--- components/public/Page.vue
+++ components/public/Page.vue
@@ -1,6 +1,6 @@
 <template>
     <main
-        class="shadow-lg border border-gray-200 rounded-md bg-white p-5 xl:p-8 2xl:p-10 min-h-[50vh] mx-auto max-w-screen-2xl"
+        class="my-8 shadow-lg border border-gray-200 rounded-md bg-white p-5 xl:p-8 2xl:p-10 min-h-[50vh] mx-auto max-w-screen-2xl"
     >
         <PublicBreadcrumb :add-concept-link="false" v-if="showBreadcrumb" />
         <UIPageTitle class="border-b-2 border-gray-200 pb-1 mb-4" v-if="title">
