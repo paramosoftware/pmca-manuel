963e74450f547ab0ac821b9c14a38c7e74a781d8
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-10 17:23:45 -0300

    feat: persistence on selected navigation mode

--- components/public/HierarchicalNavigation.vue
+++ components/public/HierarchicalNavigation.vue
@@ -1,96 +1,86 @@
-:
 <template>
-    <div id="containerPointer" class="flex h-full max-w-full w-auto">
-        <aside
-            id="tree-navigation"
-            :class="
-                closeClass +
-                ' hover:' +
-                openClass +
-                'rounded-tl-lg rounded-bl-lg shadow-lg border w-full bg-gray-200 border-gray-300 transition-all duration-300 max-h-screen  overflow-auto'
-            "
-            v-if="1 + 1"
-            ref="treeNavigationRef"
+    <div
+        id="hierarchicalWrapper"
+        class="flex flex-col h-full bg-gray-200 rounded-tl-lg rounded-bl-lg"
+    >
+        <div
+            id="firstRowHierarchical"
+            :class="[
+                ' rounded-tl-lg justify-center p-4 flex flex-row',
+                { 'bg-gray-200': !navigationStore.hierarchical.isOpen },
+                {
+                    'bg-gray-50 border-t border-l border-b border-gray-200 ':
+                        navigationStore.hierarchical.isHovered ||
+                        navigationStore.hierarchical.isOpen ||
+                        navigationStore.hierarchical.isPinned
+                }
+            ]"
         >
-            <div class="flex py-4 pl-4 pr-8 h-full">
-                <div class="flex flex-col w-full h-full">
-                    <div class="flex justify-between items-center">
-                        <div
-                            class="flex items-center"
-                            v-show="
-                                isTreeNavigationOpen || isTreeNavigationPinned
-                            "
-                        >
-                            <UIIcon
-                                name="ph:push-pin"
-                                :class="{
-                                    'text-pmca-accent': isTreeNavigationPinned
-                                }"
-                                class="mr-3 hover:text-pmca-accent w-6 h-6"
-                                title="Fixar"
-                                @click="pinTreeNavigation"
-                            />
-                            <div class="text-lg font-semibold pb-2">
-                                Classificação
-                            </div>
-                        </div>
-                        <UIIcon
-                            name="ph:tree-structure"
-                            class="ml-auto"
-                            cursor-class=""
-                        />
-                    </div>
-                    <div
-                        class="overflow-auto h-full"
-                        v-show="isTreeNavigationOpen || isTreeNavigationPinned"
-                    >
-                        <UITreeView
-                            :tree="conceptsTree"
-                            :concept-store="
-                                useConceptStoreForTree
-                                    ? conceptStore
-                                    : undefined
-                            "
-                            v-if="conceptsTree.length > 0"
-                            class="pt-2"
-                        />
-                    </div>
-                </div>
-            </div>
-        </aside>
-
-        <aside v-else>
-            <USlideover
-                v-model="isTreeNavigationOpen"
-                class="text-pmca-primary"
-                side="left"
-                :ui="{
-                    background: 'bg-gray-100'
-                }"
+            <UIIcon
+                v-show="navigationStore.isHierarchicalViewOpen"
+                name="ph:push-pin"
+                :class="[
+                    ' hover:text-pmca-accent w-6 h-6 m-4',
+                    {
+                        'text-pmca-accent':
+                            navigationStore.hierarchical.isPinned
+                    },
+                    hierarchicalOpenHoverConditionals
+                ]"
+                title="Fixar"
+                @click="navigationStore.pinHierarchicalView"
+            />
+            <div
+                :class="[
+                    'text-lg font-semibold ml-4 my-4',
+                    hierarchicalOpenHoverConditionals
+                ]"
             >
-                <div class="p-4 overflow-x-auto">
-                    <div class="flex flex-row justify-between">
-                        <div class="text-lg font-semibold">Classificação</div>
+                Classificação
+            </div>
+            <UIIcon name="ph:tree-structure" class="my-4 block w-24 h-8" />
+        </div>
 
-                        <UIIcon
-                            name="ph:x"
-                            class="ml-auto"
-                            title="Fechar navegação"
-                            @click="
-                                isTreeNavigationOpen = !isTreeNavigationOpen
-                            "
-                        />
-                    </div>
+        <UITreeView
+            :tree="conceptsTree"
+            :concept-store="useConceptStoreForTree ? conceptStore : undefined"
+            v-if="conceptsTree.length > 0"
+            :class="[
+                'hidden md:block lg:block pt-2 mx-8 h-max-full',
+                hierarchicalOpenHoverConditionals
+            ]"
+        />
+    </div>
 
-                    <UITreeView
-                        :tree="conceptsTree"
-                        class="mt-6"
-                        v-if="conceptsTree.length > 0"
+    <USlideover
+    v-if="navigationStore.screen.isSmall"
+            v-model="navigationStore.hierarchical.isOpen"
+            class="md:hidden lg:hidden text-pmca-primary"
+            side="left"
+            :ui="{
+                background: 'bg-gray-100'
+            }"
+        >
+            <div class="p-4 overflow-x-auto">
+                <div class="flex flex-row justify-between">
+                    <div class="text-lg font-semibold">Classificação</div>
+
+                    <UIIcon
+                        name="ph:x"
+                        class="ml-auto"
+                        title="Fechar navegação"
+                        @click="navigationStore.toggleOpen"
                     />
                 </div>
-            </USlideover>
-        </aside>
-    </div>
+
+                <UITreeView
+                    :tree="conceptsTree"
+                    class="mt-6"
+                    v-if="conceptsTree.length > 0"
+                />
+            </div>
+        </USlideover>
+        
 </template>
 
 <script setup lang="ts">
@@ -101,111 +91,25 @@ defineProps({
     }
 });
 
-const conceptStore = useConceptStore();
-await conceptStore.fetchConceptsTree();
-
-const { conceptsTree } = storeToRefs(conceptStore);
-
-const isSmallScreen = ref(false);
-const isTreeNavigationPinned = ref(true);
-const isTreeNavigationOpen = ref(false);
-const treeNavigationRef = ref<HTMLElement | null>(null);
-const mainRef = ref<HTMLElement | null>(null);
-const mainDivClassOpen = 'xl:w-[74%]';
-const mainDivClassClose = 'xl:w-[95%]';
-const closeClass = 'w-[5%]';
-const openClass = 'w-[24%]';
-const localStorageKey = 'isTreeNavigationFixed';
-const screenBreakpoint = 1280;
-
-const openTreeNavigation = (open: boolean) => {
-    if (isSmallScreen.value) {
-        console.log('aa');
-        isTreeNavigationOpen.value = open;
-        return;
-    }
-
-    if (!treeNavigationRef.value) {
-        treeNavigationRef.value = document.getElementById('tree-navigation');
-        if (!treeNavigationRef.value) {
-            console.error('tree navigation not found');
-            return;
-        }
-    }
-
-    if (!mainRef.value) {
-        mainRef.value = document.getElementById('main-navigation');
-        if (!mainRef.value) {
-            console.error('main not found');
-            return;
-        }
-    }
-
-    isTreeNavigationOpen.value = open;
-
-    if (open || isTreeNavigationPinned.value) {
-        treeNavigationRef.value.classList.remove(closeClass);
-        treeNavigationRef.value.classList.add(openClass);
-        mainRef.value.classList.remove(mainDivClassClose);
-        mainRef.value.classList.add(mainDivClassOpen);
-        return;
-    } else {
-        treeNavigationRef.value.classList.remove(openClass);
-        treeNavigationRef.value.classList.add(closeClass);
-        mainRef.value.classList.remove(mainDivClassOpen);
-        mainRef.value.classList.add(mainDivClassClose);
-        return;
-    }
-};
-
-const pinTreeNavigation = () => {
-    const pinLocal = localStorage.getItem(localStorageKey);
-    if (!pinLocal || pinLocal === 'false') {
-        localStorage.setItem(localStorageKey, 'true');
-        isTreeNavigationPinned.value = true;
-    } else {
-        localStorage.setItem(localStorageKey, 'false');
-        isTreeNavigationPinned.value = false;
-    }
-};
-
-onMounted(() => {
-    isSmallScreen.value = window.innerWidth < screenBreakpoint;
-    isTreeNavigationPinned.value =
-        localStorage.getItem(localStorageKey) === 'true';
-    openTreeNavigation(isTreeNavigationPinned.value && !isSmallScreen.value);
-    addHoverListener();
-
-    window.addEventListener('resize', async () => {
-        isSmallScreen.value = window.innerWidth < screenBreakpoint;
-        if (!isSmallScreen.value) {
-            await nextTick();
-            isTreeNavigationOpen.value =
-                isTreeNavigationOpen.value || isTreeNavigationPinned.value;
-            openTreeNavigation(isTreeNavigationOpen.value);
-            addHoverListener();
-        } else {
-            openTreeNavigation(false);
-        }
-    });
+defineOptions({
+    inheritAttrs: false
 });
 
-const addHoverListener = () => {
-    if (treeNavigationRef.value) {
-        treeNavigationRef.value.addEventListener('mouseenter', () => {
-            openTreeNavigation(true);
-        });
+const hierarchicalOpenHoverConditionals = computed(() => {
+    return {
+        'hidden md:hidden lg:hidden': !navigationStore.hierarchical.isOpen,
+        'hidden md:block lg:block':
+            navigationStore.hierarchical.isHovered ||
+            navigationStore.hierarchical.isOpen ||
+            navigationStore.hierarchical.isPinned
+    };
+});
+const conceptStore = useConceptStore();
+await conceptStore.fetchConceptsTree();
+const navigationStore = useNavigationStore();
 
-        treeNavigationRef.value.addEventListener('mouseleave', () => {
-            openTreeNavigation(isTreeNavigationPinned.value);
-        });
-    }
-};
+const { conceptsTree } = storeToRefs(conceptStore);
+onMounted(() => {
+    navigationStore.validateScreenSize();
+});
 </script>
-
-<style>
-#containerPointer {
-    resize: horizontal;
-    overflow: auto;
-}
-</style>
--- components/public/NovaNav.vue
+++ components/public/NovaNav.vue
@@ -1,59 +1,116 @@
 <template>
     <div
-        class="mt-2 md:flex flex-col items-stretch h-full w-full max-w-full rounded-md"
+        class="px-8 my-2 mb-4 md:flex flex-col items-stretch h-full w-full max-w-full rounded-md"
     >
-        <div id="icons" class="flex flex-row justify-end items-center">
-            <div></div>
-            <div
-                class="flex flex-row justify-center items-center bg-gray-50 h-12 mb-2 rounded-md shadow-lg border border-gray-200 p-1"
-            >
-                <UIIcon
-                    @click="
-                        saveLastSelectedNavigation(navigationModes.hierarchical)
-                    "
-                    name="ph:tree-structure"
-                    :class="{
-                        'text-black w-6 mr-4': !isHierarchical,
-                        'bg-white text-blue-500 w-6 mr-4': isHierarchical,
-                        'cursor-help': true
-                    }"
-                    title="Utilizar classificação hierárquica."
-                />
-                <UIIcon
-                    @click="
-                        saveLastSelectedNavigation(navigationModes.alphabetical)
-                    "
-                    name="ph:text-aa"
-                    :class="{
-                        'text-black w-6 mr-4': !isAlphabetical,
-                        'bg-white text-blue-500 w-6 mr-4': isAlphabetical
-                    }"
-                    title="Utilizar listagem alfabética."
-                />
-                <UIIcon
-                    @click="saveLastSelectedNavigation(navigationModes.diagram)"
-                    name="ph:align-top-simple"
-                    :class="{
-                        'text-black w-6 mr-4': !isDiagram,
-                        'bg-white text-blue-500 w-6 mr-4': isDiagram
-                    }"
-                    title="Utilizar visão de diagrama"
-                />
-            </div>
-        </div>
         <div
-            id="wrapperye"
-            class="rounded-tl-lg rounded-bl-lg flex flex-shrink-0 h-auto"
+            id="firstRow"
+            class="flex md:flex-row lg:flex-row w-full justify-between items-center md:justify-end lg:justify-end"
         >
             <div
-                v-if="isHierarchical || isDefault"
-                id="hierarchical"
-                class="w-auto max-w-[40vw]"
+                id="openHierarchical"
+                @click="navigationStore.toggleOpen"
+                :class="[
+                    'border-gray-200 border p-4 mb-2 shadow-lg rounded order-1 md:order-1 md:hidden lg:hidden lg:order-1 space-x-2 text-lg font-semibold cursor-pointer bg-gray-100',
+                    {
+                        // 'bg-gray-100': !navigationStore.hierarchical.isOpen,
+                        // 'bg-gray-200': navigationStore.hierarchical.isOpen
+                    }
+                ]"
             >
-                <PublicHierarchicalNavigation  />
+                {{
+                    navigationStore.hierarchical.isOpen
+                        ? 'Esconder classificação'
+                        : 'Abrir classificação'
+                }}
+                <UIIcon
+                    name="ph:tree-structure"
+                    class="ml-auto"
+                    cursor-class=""
+                />
             </div>
             <div
-                v-if="isAlphabetical || isDefault || isHierarchical"
+                id="icons"
+                class="order-2 md:order-2 lg:order-2 flex flex-row justify-end items-center"
+            >
+                <div></div>
+                <div
+                    class="p-8 space-x-12 flex flex-row justify-center items-center bg-gray-50 h-12 mb-2 rounded-md shadow-lg border border-gray-200"
+                >
+                    <UIIcon
+                        @click="
+                            saveLastSelectedNavigation(
+                                navigationModes.hierarchical
+                            )
+                        "
+                        name="ph:tree-structure"
+                        :class="{
+                            'text-black': !navigationStore.isHierarchical,
+                            'bg-white text-pmca-green-600 ':
+                                navigationStore.isHierarchical ||
+                                navigationStore.isDefault,
+                            'cursor-help': true
+                        }"
+                        title="Utilizar classificação hierárquica."
+                    />
+                    <UIIcon
+                        @click="
+                            saveLastSelectedNavigation(
+                                navigationModes.alphabetical
+                            )
+                        "
+                        name="ph:text-aa"
+                        :class="{
+                            'text-black': !navigationStore.isAlphabetical,
+                            'bg-white text-pmca-green-600':
+                                navigationStore.isAlphabetical
+                        }"
+                        title="Utilizar listagem alfabética."
+                    />
+                    <UIIcon
+                        @click="
+                            saveLastSelectedNavigation(navigationModes.diagram)
+                        "
+                        name="ph:align-top-simple"
+                        :class="{
+                            'text-black': !navigationStore.isDiagram,
+                            'bg-white text-pmca-green-600':
+                                navigationStore.isDiagram
+                        }"
+                        title="Utilizar visão de diagrama"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div
+            id="wrapperye"
+            class="rounded-tl-lg rounded-bl-lg flex flex-shrink-0 h-full"
+        >
+            <div
+                v-if="
+                    navigationStore.isHierarchical || navigationStore.isDefault
+                "
+                id="hierarchicalViewContainer"
+                :class="
+                    ([
+                        'hidden md:block lg:block min-w-auto w-auto max-w-[40vw]'
+                    ],
+                    {
+                        'resize-x overflow-auto max-w-[40vw]':
+                            navigationStore.isHierarchicalViewOpen
+                    })
+                "
+                @mouseenter="navigationStore.updateHoverState"
+                @mouseleave="navigationStore.updateHoverState"
+            >
+                <PublicHierarchicalNavigation class="w-full max-w-[40vw]" />
+            </div>
+            <div
+                v-if="
+                    navigationStore.isAlphabetical ||
+                    navigationStore.isDefault ||
+                    navigationStore.isHierarchical
+                "
                 id="grid"
                 class="px-8 py-8 flex-shrink flex-grow bg-gray-50 rounded-md shadow-lg border border-gray-200"
             >
@@ -61,7 +118,7 @@
             </div>
 
             <div
-                v-if="isDiagram"
+                v-if="navigationStore.isDiagram"
                 class="max-w-full p-4 flex-shrink flex-grow bg-gray-50 rounded-md shadow-lg border border-gray-200"
                 id="diagram"
             >
@@ -79,37 +136,59 @@
 
 <script setup lang="ts">
 defineProps({});
+const navigationStore = useNavigationStore();
+const localStorageKeys = {
+    navigation: {
+        lastSelected: 'lastSelectedNavigation'
+    },
+    hierarchical: {
+        isPinned: 'isPinned'
+    }
+};
 
-// Maybe see a more clean way?  Being more verbose here helps readability on template. Otherwise oyu need to put all conditionals on the v-ifs.
+const validateKeyOnLocalStorage = (key: string) => {
+    const itemOnLocalStorage = localStorage.getItem(key);
+    const validation =
+        (itemOnLocalStorage ? JSON.parse(itemOnLocalStorage) : false) === true;
+    console.log(
+        `Validating key ${key} at localStorage. Returned ${itemOnLocalStorage} with validation ${validation}`
+    );
+    return validation;
+};
+
+const initializeUserPreferencesOnStore = () => {
+    navigationStore.loadUserPreferences(
+        validateKeyOnLocalStorage(localStorageKeys.hierarchical.isPinned),
+        localStorage.getItem(localStorageKeys.navigation.lastSelected) ??
+            'default'
+    );
+};
+
+onBeforeMount(() => {});
+
 const navigationModes = {
     hierarchical: 'hierarchical',
     alphabetical: 'alphabetical',
     diagram: 'diagram',
     default: 'default'
 };
-const isHierarchical = computed(() => {
-    return navigationMode.value == navigationModes.hierarchical;
-});
-
-const isAlphabetical = computed(() => {
-    return navigationMode.value == navigationModes.alphabetical;
-});
-
-const isDiagram = computed(() => {
-    return navigationMode.value == navigationModes.diagram;
-});
-
-const isDefault = computed(() => {
-    return navigationMode.value == navigationModes.default;
-});
 
 const navigationMode = ref();
 
 const saveLastSelectedNavigation = (selectedNavigation: string) => {
     localStorage.setItem('lastSelectedNavigation', selectedNavigation);
+    navigationStore.setNavigationMode(selectedNavigation);
     navigationMode.value = selectedNavigation;
     console.log(navigationMode);
 };
+
+onMounted(() => {
+    navigationStore.validateScreenSize();
+    initializeUserPreferencesOnStore();
+    window.addEventListener('resize', () =>
+        navigationStore.validateScreenSize()
+    );
+});
 
 onBeforeMount(() => {
     navigationMode.value = localStorage.getItem('lastSelectedNavigation')
@@ -117,9 +196,3 @@ onBeforeMount(() => {
         : navigationModes.default;
 });
 </script>
-
-<style>
-#hierarchical {
-   
-}
-</style>
