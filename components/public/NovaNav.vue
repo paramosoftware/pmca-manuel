82cc18dc1dd38efdaa63860526d5d45a339feb66
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-12 12:10:57 -0300

    fix: new navigation component now passing navigationToggler within children components

--- components/public/NovaNav.vue
+++ components/public/NovaNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="lg:px-8 my-2 mb-4 md:flex flex-col items-stretch h-full w-full max-w-full rounded-md"
+        class="lg:px-8 my-8 md:flex flex-col items-stretch h-full w-full max-w-full rounded-md"
     >
         <div
             id="firstRow"
@@ -10,11 +10,7 @@
                 id="openHierarchical"
                 @click="navigationStore.onOpenHierarchicalButton()"
                 :class="[
-                    'border-gray-200 border p-4 mb-2 shadow-lg rounded order-2 md:order-1 md:hidden lg:hidden lg:order-1 space-x-2 text-lg font-semibold cursor-pointer bg-gray-100',
-                    {
-                        // 'bg-gray-100': !navigationStore.hierarchical.isOpen,
-                        // 'bg-gray-200': navigationStore.hierarchical.isOpen
-                    }
+                    'border-gray-200 border p-4 mb-2 shadow-lg rounded order-2 md:order-1 md:hidden lg:hidden lg:order-1 space-x-2 text-lg font-semibold cursor-pointer bg-gray-100'
                 ]"
             >
                 {{
@@ -27,57 +23,6 @@
                     class="ml-auto"
                     cursor-class=""
                 />
-            </div>
-            <div
-                id="icons"
-                class="order-1 md:order-2 lg:order-2 flex flex-row justify-end items-center"
-            >
-                <div
-                    class="p-8 space-x-12 flex flex-row justify-center items-center bg-gray-50 h-12 mb-2 rounded-md shadow-lg border border-gray-200"
-                >
-                    <UIIcon
-                        @click="
-                            saveLastSelectedNavigation(
-                                navigationModes.hierarchical
-                            )
-                        "
-                        name="ph:tree-structure"
-                        :class="{
-                            'text-black': !navigationStore.isHierarchical,
-                            'bg-white text-pmca-green-600 ':
-                                navigationStore.isHierarchical ||
-                                navigationStore.isDefault,
-                            'cursor-help': true
-                        }"
-                        title="Utilizar classificação hierárquica."
-                    />
-                    <UIIcon
-                        @click="
-                            saveLastSelectedNavigation(
-                                navigationModes.alphabetical
-                            )
-                        "
-                        name="ph:text-aa"
-                        :class="{
-                            'text-black': !navigationStore.isAlphabetical,
-                            'bg-white text-pmca-green-600':
-                                navigationStore.isAlphabetical
-                        }"
-                        title="Utilizar listagem alfabética."
-                    />
-                    <UIIcon
-                        @click="
-                            saveLastSelectedNavigation(navigationModes.diagram)
-                        "
-                        name="ph:align-top-simple"
-                        :class="{
-                            'text-black': !navigationStore.isDiagram,
-                            'bg-white text-pmca-green-600':
-                                navigationStore.isDiagram
-                        }"
-                        title="Utilizar visão de diagrama"
-                    />
-                </div>
             </div>
         </div>
 
@@ -110,18 +55,21 @@
                 id="grid"
                 class="px-8 max-w-full py-8 flex-shrink flex-grow bg-gray-50 rounded-md shadow-lg border border-gray-200"
             >
-                <PublicGrid class="flex-shrink-0" />
+                <PublicGrid class="flex-shrink-0">
+                    <PublicNavigationToggler />
+                </PublicGrid>
             </div>
 
             <div
                 v-if="navigationStore.isDiagram"
-                class="max-w-full p-4 flex-shrink flex-grow bg-gray-50 rounded-md shadow-lg border border-gray-200"
+                class="max-w-full p-8 flex-shrink flex-grow bg-gray-50 rounded-md shadow-lg border border-gray-200"
                 id="diagram"
             >
                 <div
-                    class="flex items-center justify-center flex-row h-auto max-w-full"
+                    class="flex flex-col items-center justify-center h-auto max-w-full"
                 >
-                    <p>
+                    <PublicNavigationToggler class="self-end mb-4" />
+                    <p class="order-2">
                         <img src="../../data/media/diagram.png" class="h-4/5" />
                     </p>
                 </div>
@@ -132,7 +80,10 @@
 
 <script setup lang="ts">
 defineProps({});
+
 const navigationStore = useNavigationStore();
+
+// Maybe move those to utils?
 const localStorageKeys = {
     navigation: {
         lastSelected: 'lastSelectedNavigation'
@@ -159,25 +110,7 @@ const initializeUserPreferencesOnStore = () => {
             'default'
     );
 };
-
-onBeforeMount(() => {});
-
-const navigationModes = {
-    hierarchical: 'hierarchical',
-    alphabetical: 'alphabetical',
-    diagram: 'diagram',
-    default: 'default'
-};
-
-const navigationMode = ref();
-
-const saveLastSelectedNavigation = (selectedNavigation: string) => {
-    localStorage.setItem('lastSelectedNavigation', selectedNavigation);
-    navigationStore.setNavigationMode(selectedNavigation);
-    navigationMode.value = selectedNavigation;
-    console.log(navigationMode);
-};
-
+//
 onMounted(() => {
     navigationStore.validateScreenSize();
     initializeUserPreferencesOnStore();
@@ -185,13 +118,5 @@ onMounted(() => {
     window.addEventListener('resize', () =>
         navigationStore.validateScreenSize()
     );
-});
-
-onBeforeUnmount(() => {});
-
-onBeforeMount(() => {
-    navigationMode.value = localStorage.getItem('lastSelectedNavigation')
-        ? localStorage.getItem('lastSelectedNavigation')
-        : navigationModes.default;
 });
 </script>
