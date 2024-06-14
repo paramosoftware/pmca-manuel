273f0a08818a95d612870d25c706e28c6f8e07da
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-06-14 15:33:09 -0300

    fix: changed individual concept layout to match new navigation structure

--- pages/termos/[slug].vue
+++ pages/termos/[slug].vue
@@ -1,81 +1,115 @@
 <template>
-    <PublicNavigation :use-concept-store-for-tree="false">
-        <article class="w-7xl">
-            <PublicBreadcrumb :links="breadcrumb" />
-            <PublicFullCardTitle :id="id" :name="concept!.name" class="mb-4" />
-            <div class="flex flex-row">
-                <div class="pr-2 w-full">
-                    <UITab :tabs="['Termo', 'Histórico de alterações']">
-                        <template #tabPanel-1>
-                            <div class="flex flex-col lg:flex-row gap-5">
-                                <div v-if="conceptValidator" class="w-full">
-                                    <div
-                                        class="flex justify-center mt-4 items-center w-full h-48"
-                                    >
-                                        <p
-                                            class="text-gray-400 text-center"
-                                        >
-                                            Nenhuma informação cadastrada.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div
-                                    v-else
-                                    class="flex flex-col min-h-48 w-full lg:w-8/12"
-                                >
-                                    <PublicFieldAttribute
-                                        title="Definição"
-                                        :content="concept?.definition"
-                                        :is-html="true"
-                                    />
-
-                                    <PublicFieldAttribute
-                                        title="Notas"
-                                        :content="concept?.notes"
-                                        :is-html="true"
-                                    />
-
-                                    <PublicFieldAttribute
-                                        title="Formas variantes"
-                                        :content="concept?.variations"
-                                    />
-
-                                    <PublicFieldAttribute
-                                        title="Termos equivalentes em outros idiomas"
-                                        :content="translations"
-                                    />
-
-                                    <PublicFieldAttribute
-                                        title="Fontes"
-                                        :content="concept?.references"
-                                        :is-html="true"
-                                        :is-one-line="true"
-                                    />
-
-                                    <PublicRelatedConcepts
-                                        title="Ver também"
-                                        :concepts="concept?.relatedConcepts"
-                                        :opposite-side="concept?.concepts"
-                                    />
-                                </div>
-                                <div
-                                    class="w-full lg:w-4/12 order-first lg:order-last py-auto bg-gray-100 rounded-md"
-                                    v-if="images.length > 0"
-                                >
-                                    <PublicMedia :images="images" />
-                                </div>
-                            </div>
-                        </template>
-                        <template #tabPanel-2>
-                            <div class="flex flex-col">
-                                <PublicHistoryChange />
-                            </div>
-                        </template>
-                    </UITab>
-                </div>
+    <div
+        id="individualConceptContainer"
+        class="px-4 lg:px-8 md:mt-4 md:mb-6 md:flex flex-col items-stretch min-h-full w-screen max-w-full rounded-md"
+    >
+        <div id="wrapper" class="flex w-full min-h-full max-w-full ">
+            <div
+                id="one"
+                :style="{
+                    'max-height': `${conceptContainerCurrentHeight / 16}rem`
+                }"
+            >
+                <PublicHierarchicalNavigation
+                    class="max-w-full overflow-x-hidden"
+                />
             </div>
-        </article>
-    </PublicNavigation>
+            <div id="two" class="flex-grow h-full">
+                <article
+                    class="w-7xl p-4 md:p-8 rounded-tr-lg rounded-br-lg border-t border-l border-b border-r border-gray-200 transition-all duration-300"
+                    ref="conceptContainer"
+                >
+                    <PublicBreadcrumb :links="breadcrumb" />
+                    <PublicFullCardTitle
+                        :id="id"
+                        :name="concept!.name"
+                        class="mb-4"
+                    />
+                    <div class="flex flex-row">
+                        <div class="pr-2 w-full">
+                            <UITab :tabs="['Termo', 'Histórico de alterações']">
+                                <template #tabPanel-1>
+                                    <div
+                                        class="flex flex-col lg:flex-row gap-5"
+                                    >
+                                        <div
+                                            v-if="conceptValidator"
+                                            class="w-full"
+                                        >
+                                            <div
+                                                class="flex justify-center mt-4 items-center w-full h-48"
+                                            >
+                                                <p
+                                                    class="text-gray-400 text-center"
+                                                >
+                                                    Nenhuma informação
+                                                    cadastrada.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div
+                                            v-else
+                                            class="flex flex-col min-h-48 w-full lg:w-8/12"
+                                        >
+                                            <PublicFieldAttribute
+                                                title="Definição"
+                                                :content="concept?.definition"
+                                                :is-html="true"
+                                            />
+
+                                            <PublicFieldAttribute
+                                                title="Notas"
+                                                :content="concept?.notes"
+                                                :is-html="true"
+                                            />
+
+                                            <PublicFieldAttribute
+                                                title="Formas variantes"
+                                                :content="concept?.variations"
+                                            />
+
+                                            <PublicFieldAttribute
+                                                title="Termos equivalentes em outros idiomas"
+                                                :content="translations"
+                                            />
+
+                                            <PublicFieldAttribute
+                                                title="Fontes"
+                                                :content="concept?.references"
+                                                :is-html="true"
+                                                :is-one-line="true"
+                                            />
+
+                                            <PublicRelatedConcepts
+                                                title="Ver também"
+                                                :concepts="
+                                                    concept?.relatedConcepts
+                                                "
+                                                :opposite-side="
+                                                    concept?.concepts
+                                                "
+                                            />
+                                        </div>
+                                        <div
+                                            class="w-full lg:w-4/12 order-first lg:order-last py-auto bg-gray-100 rounded-md"
+                                            v-if="images.length > 0"
+                                        >
+                                            <PublicMedia :images="images" />
+                                        </div>
+                                    </div>
+                                </template>
+                                <template #tabPanel-2>
+                                    <div class="flex flex-col">
+                                        <PublicHistoryChange />
+                                    </div>
+                                </template>
+                            </UITab>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -157,7 +191,7 @@ const translations = ref<{ name: string; link: string }[]>([]);
 
 if (concept.value?.media) {
     concept.value.media.forEach((media: ConceptMedia) => {
-        images.value.push({ src: media.name!, alt: media.subtitle ?? ''});
+        images.value.push({ src: media.name!, alt: media.subtitle ?? '' });
     });
 }
 
@@ -211,5 +245,27 @@ useHead({
         },
         { hid: 'og:image', property: 'og:image', content: url.value }
     ]
+});
+
+const conceptContainer: Ref<HTMLElement | null> = ref(null);
+const conceptContainerCurrentHeight = ref(0);
+let conceptContainerResizeObserver: ResizeObserver;
+
+onMounted(() => {
+    if (conceptContainer.value?.offsetHeight) {
+        conceptContainerResizeObserver = new ResizeObserver(() => {
+            if (conceptContainer.value) {
+                conceptContainerCurrentHeight.value =
+                    conceptContainer.value.offsetHeight;
+            }
+        });
+        conceptContainerResizeObserver.observe(conceptContainer.value);
+    }
+});
+
+onUnmounted(() => {
+    if (conceptContainerResizeObserver && conceptContainer.value) {
+        conceptContainerResizeObserver.unobserve(conceptContainer.value);
+    }
 });
 </script>
