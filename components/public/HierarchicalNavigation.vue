79f7924f6d54721ebcaa431f8ad9b6c023dfc736
Matheus <dmatheusps@gmail.com>
2024-06-19 13:41:18 -0300

    refactor(components): remove slide-over navigation from HierarchicalNavigation

--- components/public/HierarchicalNavigation.vue
+++ components/public/HierarchicalNavigation.vue
@@ -13,39 +13,6 @@
             />
         </div>
     </div>
-
-    <USlideover
-        v-if="navigationStore.screen.isSmall"
-        v-model="navigationStore.isSlideOverOpen"
-        class="text-pmca-primary h-screen"
-        side="left"
-        :ui="{
-            background: 'bg-gray-100'
-        }"
-    >
-        <div class="p-4 h-full">
-            <div class="flex flex-row justify-between">
-                <div class="text-lg font-semibold">Classificação</div>
-
-                <UIIcon
-                    name="ph:x"
-                    class="ml-auto"
-                    title="Fechar navegação"
-                    @click="navigationStore.toggleSlideOver"
-                />
-            </div>
-            <div id="treeViewWrapper" class="w-full h-5/6 h-max-[4/5]">
-                <UITreeView
-                    :tree="conceptsTree"
-                    :concept-store="
-                    useConceptStoreForTree ? conceptStore : undefined
-                "
-                    class="mt-6 overflow-y-auto overflow-x-auto w-full max-h-full"
-                    v-if="conceptsTree.length > 0"
-                />
-            </div>
-        </div>
-    </USlideover>
 </template>
 
 <script setup lang="ts">
