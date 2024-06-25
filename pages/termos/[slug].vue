80c0dbe58628133f35dfbf876eab65fb5203ee3f
Matheus <dmatheusps@gmail.com>
2024-06-24 23:57:41 -0300

    fix(pages): load concept data in [slug] page

--- pages/termos/[slug].vue
+++ pages/termos/[slug].vue
@@ -6,4 +6,10 @@
 definePageMeta({
     layout: 'public'
 });
+
+const router = useRouter();
+const slug = ref(router.currentRoute.value.params.slug.toString());
+const conceptStore = useConceptStore();
+await conceptStore.load(slug.value);
+
 </script>
