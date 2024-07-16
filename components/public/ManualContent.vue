8026f0a41e44d167bcee4853ae2969f22d977bac
smllb <samuel.moreira6@fatec.sp.gov.br>
2024-07-16 17:01:44 -0300

    chore: nuxt content development prototype (marked for removal)

---
+++ components/public/ManualContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-2/3 h-full">
+        <div v-if="currentManualPath">
+            Content:
+            <ContentDoc :path="currentManualPath" />
+            path:
+            {{ currentManualPath }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const currentManualPath = ref(route.path.split('/').slice(2).join('/'));
+</script>
---
+++ pages/manual/[...slug].vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="h-full w-full text-app-primary bg-gray-200 flex-grow flex">
+           <UIManualNavBar class="p-4"/>
+           <PublicManualContent class="p-4"/>
+         
+    </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'public'
+});
+
+
+const route = useRoute();
+const currentPath = computed(() => {
+    return route.path;
+})
+
+// const queryParam = route.query;
+</script>
